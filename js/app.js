69457e0e71ac7ec97d847443f7bc7d735764b6bc
Chayti Saha <chaytisaha98@gmail.com>
2021-09-13 10:27:47 +0600

    card style updated

--- js/app.js
+++ js/app.js
@@ -9,20 +9,33 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.image;
+
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
+
+    div.innerHTML = `
+    <div class="single-product">
+      
+    <div>
+        <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      
+      <div class="title-box">
+        <h3 class="product-title">${product.title}</h3>
+      </div>
+      
       <p>Category: ${product.category}</p>
+      
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn" style="background-color:teal; color:azure;">Add to Cart</button>
+      <button id="details-btn" class="btn btn-warning">Details</button>
+
+    </div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
